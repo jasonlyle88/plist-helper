704ddb60b63a9d376bfc7cb622faba05f7497e35
Jason Lyle <jason.lyle@oracle.com>
2024-06-04 16:54:01 -0400

    Simplification

--- src/plist_helper/_cli.py
+++ src/plist_helper/_cli.py
@@ -8,81 +8,112 @@ import datetime as _datetime
 import sys as _sys
 import textwrap as _textwrap
 import typing as _typing
+from types import MappingProxyType as _MappingProxyType
 
 from plist_helper._plist import PlistHelper as _PlistHelper
 
-__TRUTHY_VALUES = [
+__TRUTHY_VALUES = (
     "t",
     "true",
     "y",
     "yes",
     "1",
-]
+)
 
-__ALL_ARGUMENTS = {
-    "data_type": {
-        "args": {"-t", "--data-type"},
-        "kwargs": {
-            "dest": "value_data_type",
-            "help": "The data type of the provided value",
-            "choices": _PlistHelper.ENTRY_DATA_TYPES.keys(),
-            "default": "dict",
-            "required": True,
-        },
-    },
-    "entry": {
-        "args": {"-e", "--entry"},
-        "kwargs": {
-            "dest": "path",
-            "help": """The path to the object to print. \
+__ALL_ARGUMENTS = _MappingProxyType(
+    {
+        "data_type": _MappingProxyType(
+            {
+                "args": _MappingProxyType({"-t", "--data-type"}),
+                "kwargs": _MappingProxyType(
+                    {
+                        "dest": "value_data_type",
+                        "help": "The data type of the provided value",
+                        "choices": _PlistHelper.ENTRY_DATA_TYPES.keys(),
+                        "default": "dict",
+                        "required": True,
+                    },
+                ),
+            },
+        ),
+        "entry": _MappingProxyType(
+            {
+                "args": _MappingProxyType({"-e", "--entry"}),
+                "kwargs": _MappingProxyType(
+                    {
+                        "dest": "path",
+                        "help": """The path to the object to print. \
                 Can be provided 0-n times to specify root or any child""",
-            "action": "append",
-            "default": [],
-        },
+                        "action": "append",
+                        "default": [],
+                    },
+                ),
+            },
+        ),
+        "format": _MappingProxyType(
+            {
+                "args": _MappingProxyType({"-f", "--format"}),
+                "kwargs": _MappingProxyType(
+                    {
+                        "dest": "output_format",
+                        "help": "The format in which to print the plist",
+                        "choices": _PlistHelper.FILE_FORMATS.keys(),
+                        "default": "xml",
+                    },
+                ),
+            },
+        ),
+        "plist": _MappingProxyType(
+            {
+                "args": _MappingProxyType({"-p", "--plist"}),
+                "kwargs": _MappingProxyType(
+                    {
+                        "dest": "plist",
+                        "help": "The plist to act upon",
+                        "type": str,
+                        "required": True,
+                    },
+                ),
+            },
+        ),
+        "root_data_type": _MappingProxyType(
+            {
+                "args": _MappingProxyType({"-t", "--data-type"}),
+                "kwargs": _MappingProxyType(
+                    {
+                        "dest": "data_type",
+                        "help": "The data type of the root of the plist file",
+                        "choices": _PlistHelper.ENTRY_DATA_TYPES.keys(),
+                        "default": "dict",
+                    },
+                ),
+            },
+        ),
+        "sort": _MappingProxyType(
+            {
+                "args": _MappingProxyType({"-s", "--sort"}),
+                "kwargs": _MappingProxyType(
+                    {
+                        "dest": "output_sort",
+                        "help": "Sort keys in alphabetical order",
+                        "action": "store_true",
+                    },
+                ),
+            },
+        ),
+        "value": _MappingProxyType(
+            {
+                "args": _MappingProxyType({"-v", "--value"}),
+                "kwargs": _MappingProxyType(
+                    {
+                        "dest": "value",
+                        "help": "Data value to use for the action",
+                    },
+                ),
+            },
+        ),
     },
-    "format": {
-        "args": {"-f", "--format"},
-        "kwargs": {
-            "dest": "output_format",
-            "help": "The format in which to print the plist",
-            "choices": _PlistHelper.FILE_FORMATS.keys(),
-            "default": "xml",
-        },
-    },
-    "plist": {
-        "args": {"-p", "--plist"},
-        "kwargs": {
-            "dest": "plist",
-            "help": "The plist to act upon",
-            "type": str,
-            "required": True,
-        },
-    },
-    "root_data_type": {
-        "args": {"-t", "--data-type"},
-        "kwargs": {
-            "dest": "data_type",
-            "help": "The data type of the root of the plist file",
-            "choices": _PlistHelper.ENTRY_DATA_TYPES.keys(),
-            "default": "dict",
-        },
-    },
-    "sort": {
-        "args": {"-s", "--sort"},
-        "kwargs": {
-            "dest": "output_sort",
-            "help": "Sort keys in alphabetical order",
-            "action": "store_true",
-        },
-    },
-    "value": {
-        "args": {"-v", "--value"},
-        "kwargs": {
-            "dest": "value",
-            "help": "Data value to use for the action",
-        },
-    },
-}
+)
 
 __GLOBAL_ARGUMENTS = (__ALL_ARGUMENTS["plist"],)
 
@@ -102,77 +133,97 @@ __GLOBAL_ARGUMENTS = (__ALL_ARGUMENTS["plist"],)
 # TODO(@jlyle): upsert
 
 __ACTIONS = {
-    "convertFile": {
-        "main_method": _PlistHelper.convert_file,
-        "main_method_post": None,
-        "action_help": None,
-        "argument_definitions": (__ALL_ARGUMENTS["format"],),
-    },
-    "createFile": {
-        "main_method": _PlistHelper.create_empty_file,
-        "main_method_post": None,
-        "action_help": None,
-        "argument_definitions": (
-            __ALL_ARGUMENTS["format"],
-            __ALL_ARGUMENTS["root_data_type"],
-        ),
-    },
-    "delete": {
-        "main_method": _PlistHelper.delete,
-        "main_method_post": "write_to_file",
-        "action_help": None,
-        "argument_definitions": (__ALL_ARGUMENTS["entry"],),
-    },
-    "exists": {
-        "main_method": _PlistHelper.exists,
-        "main_method_post": "output_result",
-        "action_help": None,
-        "argument_definitions": (__ALL_ARGUMENTS["entry"],),
-    },
-    "getArrayLength": {
-        "main_method": _PlistHelper.get_array_length,
-        "main_method_post": "output_result",
-        "action_help": None,
-        "argument_definitions": (__ALL_ARGUMENTS["entry"],),
-    },
-    "getDictKeys": {
-        "main_method": _PlistHelper.get_dict_keys,
-        "main_method_post": "output_list",
-        "action_help": None,
-        "argument_definitions": (__ALL_ARGUMENTS["entry"],),
-    },
-    "getType": {
-        "main_method": _PlistHelper.get_type,
-        "main_method_post": "output_result",
-        "action_help": None,
-        "argument_definitions": (__ALL_ARGUMENTS["entry"],),
-    },
-    "getValue": {
-        "main_method": _PlistHelper.get,
-        "main_method_post": "output_result",
-        "action_help": None,
-        "argument_definitions": (__ALL_ARGUMENTS["entry"],),
-    },
-    "insert": {
-        "main_method": _PlistHelper.insert,
-        "main_method_post": "write_to_file",
-        "action_help": None,
-        "argument_definitions": (
-            __ALL_ARGUMENTS["entry"],
-            __ALL_ARGUMENTS["data_type"],
-            __ALL_ARGUMENTS["value"],
-        ),
-    },
-    "print": {
-        "main_method": _PlistHelper.print,
-        "main_method_post": None,
-        "action_help": None,
-        "argument_definitions": (
-            __ALL_ARGUMENTS["format"],
-            __ALL_ARGUMENTS["sort"],
-            __ALL_ARGUMENTS["entry"],
-        ),
-    },
+    "convertFile": _MappingProxyType(
+        {
+            "main_method": _PlistHelper.convert_file,
+            "main_method_post": None,
+            "action_help": None,
+            "argument_definitions": (__ALL_ARGUMENTS["format"],),
+        },
+    ),
+    "createFile": _MappingProxyType(
+        {
+            "main_method": _PlistHelper.create_empty_file,
+            "main_method_post": None,
+            "action_help": None,
+            "argument_definitions": (
+                __ALL_ARGUMENTS["format"],
+                __ALL_ARGUMENTS["root_data_type"],
+            ),
+        },
+    ),
+    "delete": _MappingProxyType(
+        {
+            "main_method": _PlistHelper.delete,
+            "main_method_post": "write_to_file",
+            "action_help": None,
+            "argument_definitions": (__ALL_ARGUMENTS["entry"],),
+        },
+    ),
+    "exists": _MappingProxyType(
+        {
+            "main_method": _PlistHelper.exists,
+            "main_method_post": "output_simple_result",
+            "action_help": None,
+            "argument_definitions": (__ALL_ARGUMENTS["entry"],),
+        },
+    ),
+    "getArrayLength": _MappingProxyType(
+        {
+            "main_method": _PlistHelper.get_array_length,
+            "main_method_post": "output_simple_result",
+            "action_help": None,
+            "argument_definitions": (__ALL_ARGUMENTS["entry"],),
+        },
+    ),
+    "getDictKeys": _MappingProxyType(
+        {
+            "main_method": _PlistHelper.get_dict_keys,
+            "main_method_post": "output_list",
+            "action_help": None,
+            "argument_definitions": (__ALL_ARGUMENTS["entry"],),
+        },
+    ),
+    "getType": _MappingProxyType(
+        {
+            "main_method": _PlistHelper.get_type,
+            "main_method_post": "output_simple_result",
+            "action_help": None,
+            "argument_definitions": (__ALL_ARGUMENTS["entry"],),
+        },
+    ),
+    "getValue": _MappingProxyType(
+        {
+            "main_method": _PlistHelper.get,
+            "main_method_post": "output_simple_result",
+            "action_help": None,
+            "argument_definitions": (__ALL_ARGUMENTS["entry"],),
+        },
+    ),
+    "insert": _MappingProxyType(
+        {
+            "main_method": _PlistHelper.insert,
+            "main_method_post": "write_to_file",
+            "action_help": None,
+            "argument_definitions": (
+                __ALL_ARGUMENTS["entry"],
+                __ALL_ARGUMENTS["data_type"],
+                __ALL_ARGUMENTS["value"],
+            ),
+        },
+    ),
+    "print": _MappingProxyType(
+        {
+            "main_method": _PlistHelper.print,
+            "main_method_post": None,
+            "action_help": None,
+            "argument_definitions": (
+                __ALL_ARGUMENTS["format"],
+                __ALL_ARGUMENTS["sort"],
+                __ALL_ARGUMENTS["entry"],
+            ),
+        },
+    ),
 }
 
 
@@ -275,29 +326,29 @@ def __setup_argparse() -> _argparse.ArgumentParser:
     return parent_parser
 
 
-def __handle_arguments(args: _argparse.Namespace) -> None:
+def __handle_arguments(args: _argparse.Namespace) -> dict:
     """Handle the arguments from argparse for the plist_helper CLI."""
     # Setup a dictionary that can be used to call the main function with
     # unpacked args and kwargs
-    plist_helper_args = []
-    plist_helper_kwargs = vars(args).copy()
+    main_method_args = []
+    main_method_kwargs = vars(args).copy()
 
-    del plist_helper_kwargs["action_name"]
+    del main_method_kwargs["action_name"]
     for global_argument in __GLOBAL_ARGUMENTS:
-        del plist_helper_kwargs[global_argument["kwargs"]["dest"]]
+        del main_method_kwargs[global_argument["kwargs"]["dest"]]
 
     action_spec = __ACTIONS[args.action_name]
 
     typed_handle = __get_original_descriptor_from_handle(action_spec["main_method"])
     if isinstance(typed_handle, __TYPES.classmethod):
-        plist_helper_kwargs["plist_file"] = args.plist
+        main_method_kwargs["plist_file"] = args.plist
     elif isinstance(typed_handle, __TYPES.function):
         try:
             plist = _PlistHelper(_PlistHelper.PLIST_INFO_TYPE_FILE, args.plist)
         except Exception:
             __exit(status=101, message="Unable to open plist file\n")
 
-        plist_helper_args = [
+        main_method_args = [
             plist,
         ]
     else:
@@ -308,12 +359,12 @@ def __handle_arguments(args: _argparse.Namespace) -> None:
             + '"\n',
         )
 
-    if "value_data_type" in plist_helper_kwargs:
+    if "value_data_type" in main_method_kwargs:
         # Verify parameter combinations
-        value_specified = "value" in plist_helper_kwargs
+        value_specified = "value" in main_method_kwargs
 
         if (
-            plist_helper_kwargs["value_data_type"] in ["array", "dict"]
+            main_method_kwargs["value_data_type"] in ["array", "dict"]
             and value_specified
         ):
             raise RuntimeError(
@@ -321,7 +372,7 @@ def __handle_arguments(args: _argparse.Namespace) -> None:
             )  # TODO(@jlyle): Generic error handling needs to be done here
 
         if (
-            plist_helper_kwargs["value_data_type"] not in ["array", "dict"]
+            main_method_kwargs["value_data_type"] not in ["array", "dict"]
             and not value_specified
         ):
             raise RuntimeError(
@@ -329,46 +380,89 @@ def __handle_arguments(args: _argparse.Namespace) -> None:
             )  # TODO(@jlyle): Generic error handling needs to be done here
 
         if not value_specified:
-            plist_helper_kwargs["value"] = ""
+            main_method_kwargs["value"] = ""
 
-        # Attempt to convert the provided value to the appropriate python type
-        # based on the provided data type
-        if plist_helper_kwargs["value_data_type"] == "array":
-            plist_helper_kwargs["value"] = []
-        elif plist_helper_kwargs["value_data_type"] == "bool":
-            plist_helper_kwargs["value"] = (
-                plist_helper_kwargs["value"].strip().lower() in __TRUTHY_VALUES
+    return {
+        "action_spec": action_spec,
+        "main_method_args": main_method_args,
+        "main_method_kwargs": main_method_kwargs,
+        "plist": plist,
+    }
+
+
+def __convert_provided_value_to_data_type(  # noqa: C901,PLR0912
+    argument_results: dict,
+) -> None:
+    if "value_data_type" not in argument_results["main_method_kwargs"]:
+        return
+
+    main_method_kwargs = argument_results["main_method_kwargs"]
+
+    value_data_type = main_method_kwargs["value_data_type"]
+
+    del main_method_kwargs["value_data_type"]
+
+    # Attempt to convert the provided value to the appropriate python type
+    # based on the provided data type
+    if value_data_type == "array":
+        main_method_kwargs["value"] = []
+
+    elif value_data_type == "bool":
+        main_method_kwargs["value"] = (
+            main_method_kwargs["value"].strip().lower() in __TRUTHY_VALUES
+        )
+
+    elif value_data_type == "data":
+        try:
+            main_method_kwargs["value"] = main_method_kwargs["value"].encode()
+        except AttributeError as e:
+            raise e  # TODO(@jlyle): Generic error handling needs to be done here
+
+    elif value_data_type == "date":
+        try:
+            main_method_kwargs["value"] = _datetime.datetime.fromisoformat(
+                main_method_kwargs["value"],
             )
-        elif plist_helper_kwargs["value_data_type"] == "data":
-            plist_helper_kwargs["value"] = plist_helper_kwargs["value"].encode()
-        elif plist_helper_kwargs["value_data_type"] == "date":
-            try:
-                plist_helper_kwargs["value"] = _datetime.datetime.fromisoformat(
-                    plist_helper_kwargs["value"],
-                )
-            except ValueError as e:
-                raise e  # TODO(@jlyle): Generic error handling needs to be done here
-        if plist_helper_kwargs["value_data_type"] == "dict":
-            plist_helper_kwargs["value"] = {}
-        elif plist_helper_kwargs["value_data_type"] == "integer":
-            try:
-                plist_helper_kwargs["value"] = int(plist_helper_kwargs["value"])
-            except ValueError as e:
-                raise e  # TODO(@jlyle): Generic error handling needs to be done here
-        elif plist_helper_kwargs["value_data_type"] == "real":
-            try:
-                plist_helper_kwargs["value"] = float(plist_helper_kwargs["value"])
-            except ValueError as e:
-                raise e  # TODO(@jlyle): Generic error handling needs to be done here
-        elif plist_helper_kwargs["value_data_type"] == "string":
-            pass  # Value will already be a string, nothing to do
+        except ValueError as e:
+            raise e  # TODO(@jlyle): Generic error handling needs to be done here
 
-        del plist_helper_kwargs["value_data_type"]
+    elif value_data_type == "dict":
+        main_method_kwargs["value"] = {}
+
+    elif value_data_type == "integer":
+        try:
+            main_method_kwargs["value"] = int(main_method_kwargs["value"])
+        except ValueError as e:
+            raise e  # TODO(@jlyle): Generic error handling needs to be done here
+
+    elif value_data_type == "real":
+        try:
+            main_method_kwargs["value"] = float(main_method_kwargs["value"])
+        except ValueError as e:
+            raise e  # TODO(@jlyle): Generic error handling needs to be done here
+
+    elif value_data_type == "string":
+        try:
+            main_method_kwargs["value"] = str(main_method_kwargs["value"])
+        except ValueError as e:
+            raise e  # TODO(@jlyle): Generic error handling needs to be done here
+
+    else:
+        raise ValueError(
+            f"Unexpected data type '{value_data_type}'",
+        )  # TODO(@jlyle): Generic error handling needs to be done here
+
+
+def __execute_main_method(argument_results: dict) -> None:
+    action_spec = argument_results["action_spec"]
+    main_method_args = argument_results["main_method_args"]
+    main_method_kwargs = argument_results["main_method_kwargs"]
+    plist = argument_results["plist"]
 
     result = None
     execution_exception = None
     try:
-        result = action_spec["main_method"](*plist_helper_args, **plist_helper_kwargs)
+        result = action_spec["main_method"](*main_method_args, **main_method_kwargs)
     except Exception as e:
         execution_exception = e
 
@@ -380,7 +474,7 @@ def __handle_arguments(args: _argparse.Namespace) -> None:
     elif action_spec["main_method_post"] == "output_list":
         for entry in result:
             _sys.stdout.write(str(entry) + "\n")
-    elif action_spec["main_method_post"] == "output_result":
+    elif action_spec["main_method_post"] == "output_simple_result":
         if isinstance(result, bool):
             # return inverse int because false = 0 but 0 is successful exit code
             __exit(int(not result))
@@ -400,6 +494,10 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    __handle_arguments(args)
+    argument_results = __handle_arguments(args)
+
+    __convert_provided_value_to_data_type(argument_results)
+
+    __execute_main_method(argument_results)
 
     __exit(0)
